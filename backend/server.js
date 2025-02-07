58ffce0f26489a10ce11a836cd3ec61bb85ab325
Adi Jain <114348511+Adij003@users.noreply.github.com>
2025-02-07 19:45:50 +0530

    Authentication and authorization backend done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('Server....');
+const express = require('express')
+const colors = require('colors')
+
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const app = express()
+
+connectDB()
+ 
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'welcome to the support desk API'})
+}) 
+
+app.use('/api/users', require('./routes/userRoutes'))
+// app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
